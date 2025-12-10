73ceec61d7b809c78a95e0648b921273afa1c7f0
iipratte <iipratte@byu.edu>
2025-12-09 21:31:41 -0700

    removed https stuff

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# .platform/hooks/postdeploy/00_get_certificate.sh
-sudo certbot -n -d mcminnville-pallet-env.eba-an9y2jq6.us-east-2.elasticbeanstalk.com --nginx --agree-tos --email isaac.pratte@gmail.com
